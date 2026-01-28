a21d8cbc25235c09f584d947140b2bff17224480
Vaishali Sharma <vaishalis2430@gmail.com>
2026-01-29 00:13:17 +0530

    Update genai.py

--- genai.py
+++ genai.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import shapiro
+import plotly.express as px
 
 # =========================================================
 # PAGE CONFIG
