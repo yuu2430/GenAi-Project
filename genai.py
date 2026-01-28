610d979bf65b5b64d702dca9f226bcb62d5d690a
Vaishali Sharma <vaishalis2430@gmail.com>
2026-01-28 21:27:44 +0530

    Update genai.py

--- genai.py
+++ genai.py
@@ -298,7 +298,7 @@ elif active_tab == "📊 Data Visualization":
         st.subheader("Usage of AI Tools Across Academic Purposes")
 
         df = pd.read_excel(
-            "FINAL DATA OF PROJECT.xlsx",
+            "FINAL DATA OF PROJECT (1).xlsx",
             sheet_name="Sheet3"
         )
 
@@ -497,7 +497,7 @@ elif active_tab == "📑 Tests":
     st.subheader("Normality Assessment of AI Dependency Score")
 
     dep_df = pd.read_excel(
-        "FINAL DATA OF PROJECT.xlsx",
+        "FINAL DATA OF PROJECT (1).xlsx",
         sheet_name="Sheet5"
     )
 
