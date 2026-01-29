2e708fdf0e41dec08f487d19dc6cc72c0130643a
Vaishali Sharma <vaishalis2430@gmail.com>
2026-01-30 02:04:16 +0530

    Update genai.py

--- genai.py
+++ genai.py
@@ -1066,14 +1066,7 @@ elif active_tab == "📌 Conclusion":
 
 
     
-    st.markdown("---")
-    st.markdown(
-        "<div style='text-align:center; font-size:42px; color:black; margin-top:10px;'>"
-        "<b>Thank you! :)</b>"
-        "</div>",
-        unsafe_allow_html=True
-    )
-
+    
 #references
 elif active_tab == "📚 References":
 
@@ -1134,6 +1127,15 @@ Karwowski, M., Lebuda, I., &amp; Wiśniewska, E. (2018).
     )
 
     st.markdown("<br>", unsafe_allow_html=True)
+    
+    st.markdown("---")
+    st.markdown(
+        "<div style='text-align:center; font-size:42px; color:black; margin-top:10px;'>"
+        "<b>Thank you! :)</b>"
+        "</div>",
+        unsafe_allow_html=True
+    )
+
 
     
 
